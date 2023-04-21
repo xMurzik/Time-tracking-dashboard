228d0d31247ab6c41780a162a46a278b3ba80029
xMurzik <murzik6667@gmail.com>
2023-04-21 14:10:48 +0300

    add chaning info about person

--- src/components/ChildElem/ChildElem.tsx
+++ src/components/ChildElem/ChildElem.tsx
@@ -6,8 +6,46 @@ interface infoCard extends Idata {
   status: string;
 }
 
+interface fixedData {
+  current: number;
+  previous: number;
+  status: string;
+}
+
 const ChildElem: React.FC<infoCard> = ({ title, path, timeframes, status }) => {
-  return <div className={styles.mainDiv}></div>;
+  let fixedData: fixedData = {} as fixedData;
+
+  switch (status) {
+    case 'Daily':
+      fixedData.current = timeframes.daily.current;
+      fixedData.previous = timeframes.daily.previous;
+      fixedData.status = 'Day';
+      break;
+    case 'Weekly':
+      fixedData.current = timeframes.weekly.current;
+      fixedData.previous = timeframes.weekly.previous;
+      fixedData.status = 'Week';
+      break;
+    case 'Monthly':
+      fixedData.current = timeframes.monthly.current;
+      fixedData.previous = timeframes.monthly.previous;
+      fixedData.status = 'Month';
+      break;
+    default:
+      break;
+  }
+
+  return (
+    <div className={styles.mainDiv}>
+      <span className={`${styles.divInfo} ${styles.infoName}`}>{title}</span>
+      <span className={`${styles.divInfo} ${styles.infoHours}`}>
+        {fixedData.current}hrs
+      </span>
+      <span className={`${styles.divInfo} ${styles.infoLastHours}`}>
+        Last {fixedData.status} - {fixedData.previous}hrs
+      </span>
+    </div>
+  );
 };
 
 export default ChildElem;
